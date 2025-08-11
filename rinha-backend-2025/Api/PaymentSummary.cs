d6ff9cd2f63b49ccf4ef6407de84209db8496666
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-08-10 23:31:50 -0300

    Update limits on compose.yaml

--- rinha-backend-2025/Api/PaymentSummary.cs
+++ rinha-backend-2025/Api/PaymentSummary.cs
@@ -12,8 +12,3 @@ public class PaymentSummaryItem {
     public int TotalRequests { get; set; } = 0;
     public double TotalAmount { get; set; } = 0.0;
 }
-
-public class PaymentItem {
-    public double TotalAmount { get; set; } = 0.0;
-    public DateTimeOffset RequestedAt { get; set; }
-}
